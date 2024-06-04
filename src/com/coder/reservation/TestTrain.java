a33886dc3aed854e58ff110c400a822c59a35060
Shub3023 <shubhamkale50752@gmail.com>
2024-06-04 17:15:22 +0530

    New File

---
+++ src/com/coder/reservation/TestTrain.java
@@ -0,0 +1,13 @@
+package com.coder.reservation;
+
+public class TestTrain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Train.pnrRecord p=new Train.pnrRecord();
+		System.out.println(p.getpnrNumber());
+		
+
+	}
+
+}
